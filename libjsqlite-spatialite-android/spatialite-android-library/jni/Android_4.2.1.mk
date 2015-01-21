08c44700a53ad3e21c23c50d8c088218346c8ff9
mj10777 <mj10777@googlemail.com>
2015-01-21 04:44:01 +0100

    added sqlite-3080801 files.

--- libjsqlite-spatialite-android/spatialite-android-library/jni/Android_4.2.1.mk
+++ libjsqlite-spatialite-android/spatialite-android-library/jni/Android_4.2.1.mk
@@ -9,13 +9,13 @@ JSQLITE_PATH := javasqlite-20120209
 SPATIALITE_PATH := libspatialite-4.2.1
 GEOS_PATH := geos-3.4.2
 PROJ4_PATH := proj-4.8.0
-SQLITE_PATH := sqlite-amalgamation-3080500
+SQLITE_PATH := sqlite-amalgamation-3080801
 ICONV_PATH := libiconv-1.13.1
 XML2_PATH := libxml2-2.9.1
 LZMA_PATH := xz-5.1.3alpha
 
 include $(LOCAL_PATH)/iconv-1.13.1.mk
-include $(LOCAL_PATH)/sqlite-3080500.mk
+include $(LOCAL_PATH)/sqlite-3080801.mk
 include $(LOCAL_PATH)/proj4-4.8.0.mk
 include $(LOCAL_PATH)/geos-3.4.2.mk
 include $(LOCAL_PATH)/libxml2-2.9.1.mk
--- libjsqlite-spatialite-android/spatialite-android-library/jni/Android_R4.2.1.mk
+++ libjsqlite-spatialite-android/spatialite-android-library/jni/Android_R4.2.1.mk
@@ -8,7 +8,7 @@ JSQLITE_PATH := javasqlite-20120209
 SPATIALITE_PATH := libspatialite-4.2.1
 GEOS_PATH := geos-3.4.2
 PROJ4_PATH := proj-4.8.0
-SQLITE_PATH := sqlite-amalgamation-3080500
+SQLITE_PATH := sqlite-amalgamation-3080801
 ICONV_PATH := libiconv-1.13.1
 RASTERLITE2_PATH := librasterlite2-4.2.1
 GEOTIFF_PATH := libgeotiff-1.4.0
@@ -31,7 +31,7 @@ OPENJPEG_PATH := openjpeg-2.0.0
 include $(LOCAL_PATH)/charls-1.0.mk
 include $(LOCAL_PATH)/jsqlite-R4.2.0.mk
 include $(LOCAL_PATH)/iconv-1.13.1.mk
-include $(LOCAL_PATH)/sqlite-3080500.mk
+include $(LOCAL_PATH)/sqlite-3080801.mk
 include $(LOCAL_PATH)/proj4-4.8.0.mk
 include $(LOCAL_PATH)/geos-3.4.2.mk
 include $(LOCAL_PATH)/spatialite-4.2.1.mk
